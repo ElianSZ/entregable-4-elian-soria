62b6aea9816b428975ac1ff694e191fb64ed2268
Elián Soria Zamora <eliansoria7@gmail.com>
2021-11-02 14:19:48 +0100

    Entregable_4

---
+++ Entregable 4/Assets/Scripts/PositionGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionGenerator : MonoBehaviour
+{
+        public Vector3 RandomPosition()
+    {
+        float RandomX = Random.Range(0, 8);
+        float RandomY = Random.Range(0, 8);
+        float RandomZ = Random.Range(0, 8);
+        return new Vector3(RandomX, RandomY, RandomZ);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = RandomPosition();
+        }
+    }
+}
